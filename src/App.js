28950c2cba5e8ad0563a044492fe35476f691e71
FeMonteiro <fellipemontteiro@gmail.com>
2018-10-06 17:43:59 -0300

    Separado Routes

--- src/App.js
+++ src/App.js
@@ -1,44 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Loadable from 'react-loadable';
+import Routes from './routes.js'
 import './styles/App.scss';
 
-const Loading = () => <div>Loading...</div>;
 
-const Item1 = Loadable({
-  loader: () => import('./routes/item1/index.js'),
-  loading: Loading,
-});
-
-const Item2 = Loadable({
-  loader: () => import('./routes/item2/index.js'),
-  loading: Loading,
-});
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <a href="/">Item 1</a>
-          <a href="/Item2">Item 2</a>
-          <Router>
-            <Switch>
-              <Route exact path="/" component={Item1}/>
-              <Route path="/Item2" component={Item2} />
-            </Switch>
-          </Router>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+          <Routes />
       </div>
     );
   }
---
+++ src/routes.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Loadable from 'react-loadable';
+
+const Loading = () => <div>Loading...</div>;
+
+const Item1 = Loadable({
+  loader: () => import('./routes/item1/index.js'),
+  loading: Loading,
+});
+
+const Item2 = Loadable({
+  loader: () => import('./routes/item2/index.js'),
+  loading: Loading,
+});
+const Routes = () => (
+   <Router>
+      <Switch>
+         <Route exact path="/" component={Item1}/>
+         <Route path="/Item2" component={Item2} />
+      </Switch>
+   </Router>
+);
+
+export default Routes;
