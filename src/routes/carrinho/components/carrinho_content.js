59a494cf8815afd19f67a681f0592f95fc77435b
Vitor Takara <vtakaraa@gmail.com>
2018-10-07 17:30:44 -0300

    carrinho

--- src/routes/carrinho/components/carrinho_content.js
+++ src/routes/carrinho/components/carrinho_content.js
@@ -5,15 +5,23 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 class CarrinhoContent extends React.Component {
     render() {
         return (
-    <aside className="p-relative sidebar-container">
-        <div className="sidebar-btn-close">X</div>
-        <div className="sidebar-info">
-            <h3>{this.props.produto.Nome}</h3>
-            <small>Ref: 6</small>
-            <p className="sidebar-info-desc">Chocolate Amargo 75% cacau produzido com o cacau do João Menezes. Porção de 20g.</p>
+    <aside className="p-relative carrinho-container">
+        <div className="carrinho-btn-close">X</div>
+        <div className="carrinho-card">
+            <div className="carrinho-card-img">
+                <img src="https://picsum.photos/600/600/"/>
+            </div>
+            <div className="carrinho-card-info">
+                <h5>{this.props.produto.Nome}</h5>
+                <p>Quantidade: 1</p>
+                <p>Valor: R$4,80</p>
+            </div>
         </div>
-        <p className="sidebar-price">R$ {this.props.produto.Preco}</p>
-        <button type="button" className="button-primary">Adicionar no Carrinho</button>
+        <div className="carrinho-checkout">
+            <button type="button" className="button-primary">Finalizar Compra</button>
+            <p className="m-0">Total: R$14,40</p>
+        </div>
+        <small className="mt-15 d-flex">você será redirecionado para um ambiente seguro</small>
     </aside>
     
     )}
