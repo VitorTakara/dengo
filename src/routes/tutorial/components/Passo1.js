7c3570a25bcd305816a45c09a9d48bea905280b6
FeMonteiro <fellipemontteiro@gmail.com>
2018-10-12 15:28:18 -0300

    Update src/routes/tutorial/components/Passo1.js

--- src/routes/tutorial/components/Passo1.js
+++ src/routes/tutorial/components/Passo1.js
@@ -4,8 +4,8 @@ import {Animated} from "react-animated-css";
 const Passo1 = () => (
     <Animated animationIn="fadeInRight" animationOut="fadeOut" isVisible={true}>
         <h2>COMO A LOJA AO VIVO FUNCIONA</h2>
-        <p>Você pode pedir os chocolates Dengo da mesma maneiro com que faz em nossas
-            lojas físicas, tirando dúvidas direto com nossos Especialistas e finalizando sua
+        <p>Você pode pedir os chocolates Dengo da mesma maneira com que faz em nossas
+            lojas físicas, tirando dúvidas direto com nossos especialistas e finalizando sua
             compra como no e-commerce</p>
     </Animated>
 );
