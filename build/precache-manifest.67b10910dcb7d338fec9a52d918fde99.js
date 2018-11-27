823af278e2e4d866c949462e8f276e888aefaa57
Vitor Takara <vtakaraa@gmail.com>
2018-11-27 20:54:15 -0200

    a'

---
+++ build/precache-manifest.67b10910dcb7d338fec9a52d918fde99.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = [
+  {
+    "revision": "2697bfd00c4e0a4e589f",
+    "url": "/static/js/9.2697bfd0.chunk.js"
+  },
+  {
+    "revision": "2944587f84dcd2c7d4a4",
+    "url": "/static/js/0.2944587f.chunk.js"
+  },
+  {
+    "revision": "dfaf91a487a5e69f6f63",
+    "url": "/static/js/1.dfaf91a4.chunk.js"
+  },
+  {
+    "revision": "be55344226d409d644c269bbe853ca0c",
+    "url": "/static/media/volDown.be553442.svg"
+  },
+  {
+    "revision": "80e801fc386084a06d0e",
+    "url": "/static/js/main.80e801fc.chunk.js"
+  },
+  {
+    "revision": "928f5a09eb4ddefa26c8",
+    "url": "/static/js/3.928f5a09.chunk.js"
+  },
+  {
+    "revision": "611698fd31eb142b5d729aee3fe43d60",
+    "url": "/static/media/volUp.611698fd.svg"
+  },
+  {
+    "revision": "7e299542b1162882ee6f",
+    "url": "/static/js/4.7e299542.chunk.js"
+  },
+  {
+    "revision": "2d771322e43e06b31f22",
+    "url": "/static/js/5.2d771322.chunk.js"
+  },
+  {
+    "revision": "a5d9bf34885a7c3e36c3",
+    "url": "/static/js/6.a5d9bf34.chunk.js"
+  },
+  {
+    "revision": "e0869fb83c136506fec72b983f15ff81",
+    "url": "/static/media/open-book.e0869fb8.svg"
+  },
+  {
+    "revision": "75c4c0eb629bff8cf5ec",
+    "url": "/static/js/7.75c4c0eb.chunk.js"
+  },
+  {
+    "revision": "9aa58a2254abea513022cb8f885b41cd",
+    "url": "/static/media/chat-icon.9aa58a22.svg"
+  },
+  {
+    "revision": "4d7a7c43b48802d3fce0",
+    "url": "/static/js/8.4d7a7c43.chunk.js"
+  },
+  {
+    "revision": "ccf2da43fa476e99491d959f6f9efc26",
+    "url": "/static/media/question-circle.ccf2da43.svg"
+  },
+  {
+    "revision": "1da96cb3770e66326d00",
+    "url": "/static/js/runtime~main.1da96cb3.js"
+  },
+  {
+    "revision": "ed3d65526737888c6a22fa748f1ce811",
+    "url": "/static/media/logo_dengo.ed3d6552.svg"
+  },
+  {
+    "revision": "b8d87d697d55fe706181c15088e434a3",
+    "url": "/static/media/logo_color_dengo.b8d87d69.svg"
+  },
+  {
+    "revision": "6b2c8cf80ce84ee32f9db50cc89aaa3b",
+    "url": "/static/media/bg_loja.6b2c8cf8.jpg"
+  },
+  {
+    "revision": "4dc633ee87ed4c989e31ce221186ead8",
+    "url": "/static/media/bemvindo.4dc633ee.jpg"
+  },
+  {
+    "revision": "7c7ce38ff83a672eab1963c7a2cba0af",
+    "url": "/static/media/bg_telasiniciais.7c7ce38f.jpg"
+  },
+  {
+    "revision": "b31c8af03e5c6c141b14b2efd1c1582c",
+    "url": "/static/media/shopping-basket.b31c8af0.svg"
+  },
+  {
+    "revision": "e7338b37eba12f91af62322685a3fa27",
+    "url": "/static/media/shopping-cart.e7338b37.svg"
+  },
+  {
+    "revision": "80e801fc386084a06d0e",
+    "url": "/static/css/main.39cfc6f2.chunk.css"
+  },
+  {
+    "revision": "4d7a7c43b48802d3fce0",
+    "url": "/static/css/8.2028487f.chunk.css"
+  },
+  {
+    "revision": "75c4c0eb629bff8cf5ec",
+    "url": "/static/css/7.1636792e.chunk.css"
+  },
+  {
+    "revision": "7e299542b1162882ee6f",
+    "url": "/static/css/4.8798d4ed.chunk.css"
+  },
+  {
+    "revision": "dfaf91a487a5e69f6f63",
+    "url": "/static/css/1.8798d4ed.chunk.css"
+  },
+  {
+    "revision": "bfa5380c5d66ff533aa4264c0c7bf041",
+    "url": "/index.html"
+  }
+];
